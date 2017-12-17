aa4775dfded42f37ce40ac55cf72eca98477a883
mkovachev <martin.kovachev@outlook.com>
2017-12-17 13:27:40 +0200

    CategoryService tested completely

---
+++ ShredMates.Tests/Services/CategoryServiceTest.cs
@@ -0,0 +1,102 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using ShredMates.Data;
+using ShredMates.Data.Models;
+using ShredMates.Services.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShredMates.Tests.Services
+{
+    public class CategoryServiceTest
+    {
+        public CategoryServiceTest()
+        {
+            TestStartup.GetMapper();
+        }
+
+        [Fact]
+        public async Task ByIdAsync_ShouldReturn_CorrectCategoryWithId()
+        {
+            // Arrange
+            var dbOptions = new DbContextOptionsBuilder<ShredMatesDbContext>()
+                                   .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                                   .Options;
+            var db = new ShredMatesDbContext(dbOptions);
+            var shoppingCart = new ShoppingCart();
+            var categoryService = new CategoryService(db, shoppingCart);
+
+            var products = new List<Product>()
+            {
+                ( new Product { Title = "A"}),
+                 ( new Product { Title = "B"}),
+                 ( new Product { Title = "C"})
+            };
+
+            var category = new Category
+            {
+                Id = 1,
+                Name = "Snowboard",
+                Products = products
+            };
+
+            db.Add(category);
+            db.SaveChanges();
+
+            // Act
+            var result = await categoryService.ByIdAsync(category.Id);
+
+            // Assert
+            result.Should().BeOfType(typeof(Category));
+            result.Should().NotBeNull();
+            result.Should()
+                .Match<Category>(c => 
+                                    c.Id == category.Id
+                                    && c.Name == category.Name);
+        }
+
+
+        [Fact]
+        public async Task ProductsinCategoryAsync_ShouldReturnAllProductFromCategory_OrderedByTitle()
+        {
+            // Arrange
+            var dbOptions = new DbContextOptionsBuilder<ShredMatesDbContext>()
+                                       .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                                       .Options;
+            var db = new ShredMatesDbContext(dbOptions);
+            var shoppingCart = new ShoppingCart();
+            var categoryService = new CategoryService(db, shoppingCart);
+
+            var products = new List<Product>()
+            {
+                ( new Product { Title = "A"}),
+                 ( new Product { Title = "B"}),
+                 ( new Product { Title = "C"})
+            };
+
+            var category = new Category
+            {
+                Id = 1,
+                Name = "Snowboard",
+                Products = products
+            };
+
+            db.Add(category);
+            db.SaveChanges();
+
+            // Act
+            var result = await categoryService.ProductsinCategoryAsync(category.Id);
+
+            // Assert
+            result
+                .Should()
+                .Match(r => r.ElementAt(0).Title == "A"
+                             && r.ElementAt(1).Title == "B")
+                .And
+                .HaveCount(3);
+        }
+    }
+}
---
+++ ShredMates.Tests/TestStartup.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ShredMates.Web.Infrastructure.Mapping;
+
+namespace ShredMates.Tests
+{
+    public class TestStartup
+    {
+        private static bool testInitialized = false;
+
+        public static void GetMapper()
+        {
+            if (!testInitialized)
+            {
+                Mapper.Initialize(cfg => cfg.AddProfile<AutoMapperProfile>());
+                testInitialized = true;
+            }
+        }
+    }
+}

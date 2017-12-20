eea8662108052b51a81ddc37f44e065d14176179
mkovachev <martin.kovachev@outlook.com>
2017-12-20 13:04:22 +0200

    pagination fix

---
+++ ShredMates.Data/Migrations/20171220094433_RemoveShoppingItems.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShredMates.Data.Migrations
+{
+    public partial class RemoveShoppingItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingCartItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCartItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Amount = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: true),
+                    ShoppingCartId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCartItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCartItems_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItems_ProductId",
+                table: "ShoppingCartItems",
+                column: "ProductId");
+        }
+    }
+}
---
+++ ShredMates.Services/Models/ShoppingCartItem.cs
@@ -0,0 +1,15 @@
+﻿using ShredMates.Data.Models;
+
+namespace ShredMates.Services.Models
+{
+    public class ShoppingCartItem
+    {
+        public string Id { get; set; }
+
+        public Product Product { get; set; }
+
+        public int Amount { get; set; }
+
+        public string ShoppingCartId { get; set; } 
+    }
+}

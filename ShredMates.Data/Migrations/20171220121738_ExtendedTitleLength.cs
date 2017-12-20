c8c2bd07d3b2bf0f2003a8e7fe25e560fe80782c
mkovachev <martin.kovachev@outlook.com>
2017-12-20 18:15:40 +0200

    product name length increased

---
+++ ShredMates.Data/Migrations/20171220121738_ExtendedTitleLength.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ShredMates.Data.Migrations
+{
+    public partial class ExtendedTitleLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Products",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Products",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
